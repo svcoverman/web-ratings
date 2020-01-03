1b6c65c8db55ba159ec4b21046b36fbed2b45c2d
cowankc <cowankc92@live.com>
2020-01-03 15:04:13 -0600

    added login and create user pages

--- docs/src/components/Filter/Filter.js
+++ docs/src/components/Filter/Filter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Filter.css";
 
-const Filter = props => <div className="filter">{props.children}</div>;
+const Filter = props => 
+<div className="filter">{props.children}</div>;
 
 export default Filter;
