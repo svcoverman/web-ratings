43c0989341557523a91bbfa60268d8bd4b7d3400
cowankc <cowankc92@live.com>
2020-01-04 13:38:33 -0600

    added checkboxes for filter section

--- docs/src/pages/Home.js
+++ docs/src/pages/Home.js
@@ -2,17 +2,93 @@ import React, { Component } from "react";
 import Module from "../components/module/Index"
 import Filter from "../components/Filter/Index"
 import Container from "../components/Container/Index";
+import { List, ListItem } from "../components/List";
+import Checkbox from "../components/Checkbox"
+// import API from "../utils/API";
+
+const items = [
+    'Popular',
+    'Highest Rating',
+    'Newest'
+];
+
 
 class Home extends Component {
+    state = {
+        websites: [],
+        category: "",
+        description: "",
+        link: "",
+        image: ""
+    };
+
+    componentWillMount = () => {
+        this.selectedBoxes = new Set();
+    }
+
+    toggleBox = label => {
+        if (this.selectedBoxes.has(label)) {
+            this.selectedBoxes.delete(label);
+        } else {
+            this.selectedBoxes.add(label);
+        }
+    }
+
+    handleFormSubmit = formSubmitEvent => {
+        formSubmitEvent.preventDefault();
+        for (const box of this.selectedBoxes) {
+            console.log(box, 'is chosen');
+        }
+    }
+
+    createCheckbox = label => (
+        <Checkbox
+            label={label}
+            handleCheck={this.toggleBox}
+            key={label}
+            />
+    )
+
+    renderCheckboxes = () => (
+        items.map(this.createCheckbox)
+    )
+
+    // componentDidMount() {
+    //     this.loadSites();
+    // };
+
+    // loadSites = () => {
+    //     API.getSites()
+    //         .then(res =>
+    //             this.setState({
+    //                 websites: res.data,
+    //                 category: "",
+    //                 description: "",
+    //                 link: "",
+    //                 image: "" 
+    //             })
+    //             )
+    //             .catch(err => console.log(err));
+    // };
 
     render() {
         return (
             <Container>
                 <Filter>
-                    <h3>filter selectors</h3>
+                    <form onSubmit={this.handleFormSubmit}>
+                        {this.renderCheckboxes()}
+                        <button className="btn btn default" type="submit">Apply</button>
+                    </form>
                 </Filter>
                 <Module>
                     <h3>List of Websites</h3>
+                    <List>
+                        {this.state.websites.map(website => (
+                            <ListItem key={website._id}>
+                                {website.title}
+                            </ListItem>
+                        ))}
+                    </List>
                 </Module>
             </Container>
         );
