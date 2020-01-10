ea23e3c0d3491a935f53b09a2b659b03ef5c7ad2
cowankc <cowankc92@live.com>
2020-01-09 18:29:06 -0600

    started a features description for welcome page

--- docs/src/pages/Welcome.js
+++ docs/src/pages/Welcome.js
@@ -8,16 +8,21 @@ class Welcome extends Component {
     render() {
         return (
             <Container>
-            <div class="splash-container">
-                <div class="splash">
-                    <h1 class="splash-head">Welcome to Web Rates</h1>
-                        <p class="splash-subhead">
+            <div className="splash-container">
+                <div className="splash">
+                    <h1 className="splash-head">Welcome to Web Rates</h1>
+                        <p className="splash-subhead">
                             Web Rates allows you to post and view websites from all over the web! 
                             Leave your comments and ratings for the rest of the community to view!
                         </p>
                         <p>
-                        <a href="/home" class="pure-button pure-button-primary">Get Started</a>
+                        <a href="/home" className="pure-button pure-button-primary">Get Started</a>
                         </p>
+                </div>
+            </div>
+            <div className="content-wrapper">
+                <div className="content">
+                    <h2 className="content-head is-center">Web Rates Features</h2>
                 </div>
             </div>
             
